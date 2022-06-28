d96103ea061357c5a672f0a6e2f30d154d27fbe3
Camilo Laptinho <camsanbar@gmail.com>
2022-06-28 10:46:58 -0400

    changed search view

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import InputComponent from "../components/inputComponent";
+import MangaListComponent from "../components/mangaListComponents";
 const SearchView = () => {
+  const [manga, changeManga] = useState('');
+  useEffect(()=>{
+    document.title = 'MangaApp - search';
+  },[])
   return (
-    <div>
-      <h1>Buscar</h1>
-    </div>
+    <Container fluid>
+      <h1 className="text-center mt-3">Manga search</h1>
+      <InputComponent changeManga={changeManga}/>
+      <MangaListComponent manga={manga} />
+    </Container>
   );
 }
 export default SearchView;
