1573b02f7e0e794ab0489f30af2eefe693263007
Talleres Puente Alto <tallerespa@inacapmail.cl>
2022-06-28 12:08:30 -0400

    changed manga card and list components

--- src/components/mangaCardComponent.js
+++ src/components/mangaCardComponent.js
@@ -1,6 +1,18 @@
-const MangaCardComponent = () => {
+import { Col, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+const MangaCardComponent = ({id, titleJP, titleEN, image}) => {
+  const route = `manga/${id}`
   return (
-    <h2>Found</h2>
+    <Col xs={6} md={4} lg={2} className="mt-4">
+      <Link className="text-decoration-none text-dark" to={route}>
+        <Card>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>{titleEN??titleJP}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Link>
+    </Col>
   );
 }
 export default MangaCardComponent; 
--- src/components/mangaListComponents.js
+++ src/components/mangaListComponents.js
@@ -13,17 +13,25 @@ const MangaListComponent = ({manga}) => {
   const [url, changeUrl] = useState(`https://kitsu.io/api/edge/manga?filter[text]=${manga}`)
   const {fetchData, loading} = useFecth(url, OPTIONS);
   useEffect(()=>{
-    changeUrl(`https://kitsu.io/api/edge/manga?filter[text]=${manga}`)
+    changeUrl(`https://kitsu.io/api/edge/manga?page[limit]=12&filter[text]=${manga}`)
   },[manga])
   console.log(fetchData);
   console.log(loading);
   return (
-    <Row>
+    <Row className="mt-5">
       {!loading ? 
         <h2>loading...</h2> :
         fetchData.data.length === 0 ?
           <h2>No results</h2> :
-          <MangaCardComponent />
+          fetchData.data.map(manga=>{
+            return (<MangaCardComponent 
+              key={manga.id} 
+              id={manga.id}
+              titleJP={manga.attributes.titles.en_jp}
+              titleEN={manga.attributes.titles.en}
+              image={manga.attributes.posterImage.small}
+            />)
+          })
       }
     </Row>
   );
